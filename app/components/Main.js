1dc622aa8425daa71801253db3f1a4ae1ca5fe58
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-06-21 11:07:38 -0500

    psuedocode

--- app/components/Main.js
+++ app/components/Main.js
@@ -10,14 +10,23 @@ var helpers = require("./utils/helpers");
 // Creating the Main component
 var Main = React.createClass({
 
+// psuedocode
+  // 1) on page load: a modal pops up with options to login or "guest slay" (first wire frame)
+  // 2) click guest slay redirect to pre-written workout -fin. (3rd wire frame)
+  // 3) click login redirect to google authentication
+  // 4) google login is approved (or error message thrown)
+  // 5) login approved: redirect to welcome slayer (second wire frame)
+  // 6) if user chooses time, style and clicks "ready to slay"- workout is generated from database and displayed (3rd wire frame)
+  // 7) if user chooses track redirect to tracker page (4th wire frame)
+  // 8) add a button on each page (besides login) the reditects to a workout resources list (for now straight up weblinks, will complie ASAP)
 
-
+  
   // Here we render the function
 
   render: function() {
     return (
 
-      <div className="container" style={ {backgroundColor: "white", borderStyle: "solid", borderWidth: "1px"} }>
+      <div className="jumbotron" style={ {backgroundColor: "black", color: "red", borderStyle: "solid", borderWidth: "0px", borderRadius: "25px"} }>
 
         <div className="page-header">
           <h1 className="text-center">Slayer in Training</h1>
