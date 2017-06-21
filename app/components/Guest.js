fdb5bb888a35412f5bb5858de4b3c34523647f5e
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-06-21 13:09:34 -0500

    all divs for main

--- app/components/Guest.js
+++ app/components/Guest.js
@@ -7,10 +7,10 @@ var React = require("react");
 // Helper for making AJAX requests to our API
 var helpers = require("./utils/helpers");
 
-// Creating the Main component
-var Main = React.createClass({
+// Creating the Guest component
+var Guest = React.createClass({
 
-// psuedocode
+// full app psuedocode
   // 1) on page load: a modal pops up (or separate page) with options to login or "guest slay" (first wire frame)
   // 2) click guest slay redirect to pre-written workout (3rd wire frame)
   // 3) click login redirect to google authentication
@@ -31,14 +31,11 @@ var Main = React.createClass({
           <h1 className="text-center">Slayer in Training</h1>
       </div>
     </div>
-        <div className="text-center" idName="subHeader"> <h3 idName="welcome">Welcome Slayer!</h3> 
+        <div className="text-center" idName="subHeader"> <h3 idName="welcome">Welcome Guest Slayer!</h3> 
           Ready to train? 
         </div> 
-    <div idName=  "howLong"> <p className ="text-center"> How long can you train today? [time drop down here]</p></div>
-    <div idName= "workoutType"> <p className ="text-center"> What's your focus, Slayer? [drop down here]</p></div>
-    <p className ="text-center"><button className = "btn-danger"> Ready. Set. Slay!</button> </p>
-    <p className ="text-center"><button className = "btn-danger"> Track Progress</button> </p>
-    <div idName= "track"> <p className ="text-center"> Better, faster, stronger. Outrun the demons. </p></div> 
+    <div idName=  "howLong"> <p className ="text-center"> pre selected workout here</p></div>
+
 </div>
 
     );
@@ -46,4 +43,4 @@ var Main = React.createClass({
 });
 
 // Export the component back for use in other files
-module.exports = Main;
+module.exports = Guest;
